ee4fe23fffe9ef6eaacd82117140d9cb8b552104
user.email <neelkim1989@gmail.com>
2020-05-22 12:16:44 +0500

    fix: правки по ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,35 @@
-function openEditForm() {
+const editButton = document.querySelector(".profile__edit-button"),
+  closeButton = document.querySelector(".popup__close-button"),
+  formElement = document.querySelector(".popup__container");
+let nameInput = document.querySelector(".popup__input_edit_name"),
+  positionInput = document.querySelector(".popup__input_edit_position"),
+  nameElement = document.querySelector(".profile__name"),
+  positionElement = document.querySelector(".profile__position");;
+
+//обработка открытия/закрытия окна редактирования
+function toggleEditForm() {
   const popup = document.querySelector(".popup");
-  popup.classList.add("popup_opened");
 
-  const nameValue = document.querySelector(".profile__name").textContent,
-    positionValue = document.querySelector(".profile__position").textContent;
+  if (!popup.classList.contains("popup_opened")) {
+    const nameValue = nameElement.textContent,
+      positionValue = positionElement.textContent;
 
-  let nameInput = document.querySelector(".popup__input_edit_name"),
-    positionInput = document.querySelector(".popup__input_edit_position");
+    nameInput.value = nameValue;
+    positionInput.value = positionValue;
+  }
 
-  nameInput.value = nameValue;
-  positionInput.value = positionValue;
+  popup.classList.toggle("popup_opened");
 }
 
-const editButton = document.querySelector(".profile__edit-button");
-editButton.addEventListener("click", openEditForm);
-
-function closeForm() {
-  const popup = document.querySelector(".popup");
-  popup.classList.remove("popup_opened");
-}
-const closeButton = document.querySelector(".popup__close-button");
-closeButton.addEventListener("click", closeForm);
-
-let formElement = document.querySelector(".popup__container");
-
+//Обработка сохрания данных формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = document.querySelector(".popup__input_edit_name"),
-    positionInput = document.querySelector(".popup__input_edit_position");
-
-  let nameValue = nameInput.value,
-    positionValue = positionInput.value;
-
-  const nameElement = document.querySelector(".profile__name"),
-    positionElement = document.querySelector(".profile__position");
-
-  nameElement.textContent = nameValue;
-  positionElement.textContent = positionValue;
-  closeForm();
+  nameElement.textContent = nameInput.value;
+  positionElement.textContent = positionInput.value;
+  toggleEditForm();
 }
 
+editButton.addEventListener("click", toggleEditForm);
+closeButton.addEventListener("click", toggleEditForm);
 formElement.addEventListener("submit", formSubmitHandler);
