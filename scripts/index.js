db6c5df8817bb21089841def9663138b770b06ba
user.email <neelkim1989@gmail.com>
2020-07-06 16:41:27 +0500

    fix: вынести данные карточек в отдельный файл data

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
+import initialCards from './data.js';
 
 const editButton = document.querySelector(".profile__edit-button"),
   addButton = document.querySelector(".profile__add-button"),
-  nameElement = documentgit.querySelector(".profile__name"),
+  nameElement = document.querySelector(".profile__name"),
   positionElement = document.querySelector(".profile__position"),
   popup = document.querySelector(".popup"),
   closeButton = popup.querySelector(".popup__close-button"),
@@ -15,38 +16,6 @@ const editButton = document.querySelector(".profile__edit-button"),
   addUrlInput = addFormElement.querySelector(".popup__input_add_url"),
   cardsElement = document.querySelector(".elements"),
   escapeCode = 27,
-  cards = [
-    {
-      name: "Архыз",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    },
-  ],
   configObject = {
     inputSelector: ".popup__input",
     submitButtonSelector: ".popup__save-button",
@@ -153,7 +122,7 @@ closeButton.addEventListener("click", () => {
 editFormElement.addEventListener("submit", editFormSubmitHandler);
 addFormElement.addEventListener("submit", addFormSubmitHandler);
 
-cards.forEach((card) => {
+initialCards.forEach((card) => {
   const cardObject = new Card(card, '#card-template', showPopup);
   const cardElement = cardObject.generateCard();
   cardsElement.prepend(cardElement);
