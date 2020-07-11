f64b9b726369939d55d4261c1c27fead9f1ba471
user.email <neelkim1989@gmail.com>
2020-07-12 00:48:38 +0500

    feat: добавить класс Section

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,18 @@
 class Section {
-  constructor() {
+  constructor({items, renderer}, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._containerElement = document.querySelector(containerSelector);
+  }
 
+  renderItems() {
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._containerElement.append(element);
   }
 }
 
